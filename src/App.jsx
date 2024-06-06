6d4d9c956827c424ae01de594e44c498e8252af8
b00836848 <burak.bektas@huawei.com>
2024-06-06 08:52:28 +0300

    Browser router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
 function App() {
-  return <div className="App">First commit of the project</div>;
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard/>,
+      loader:dashboardLoader,
+      errorElement:<Error/>
+    },
+  ]);
+
+  return <div className="App"><RouterProvider router={router} /></div>;
 }
 
 export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { fetchData } from "./helper"
+
+export function dashboardLoader(){
+    const userName = fetchData("userName");
+    return {userName}
+}
+
+const Dashboard = ()=>{
+    const {userName} = useLoaderData()
+    return(
+        <div>
+            <h1>{userName}</h1>
+        </div>
+    )
+}
+
+export default Dashboard
